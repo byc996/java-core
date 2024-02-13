01e84525e6d570b3ddd2ee7fc186eefdc71c41b2
byc996 <buyicode@gmail.com>
2024-02-12 23:30:37 -0500

    CompletableFuture Demo

---
+++ src/com/byc/concurrent/CompletableFutureDemo.java
@@ -0,0 +1,21 @@
+package com.byc.concurrent;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "hello";
+        }).thenApply(r -> {
+            return r + " world";
+        });
+        System.out.println(completableFuture.get());
+    }
+}
